1ea190437678ddb69836c63d71850a0bd680d84e
Niko Fernandez <ni.ko94@hotmail.com>
2019-06-11 17:18:58 -0300

    integrando la bomba

--- Sistema-Embebido/Ejemplos/INTEGRACION/INTEGRACION.ino
+++ Sistema-Embebido/Ejemplos/INTEGRACION/INTEGRACION.ino
@@ -1,47 +1,130 @@
 #include <LiquidCrystal_I2C.h> // Libreria LCD_I2C
 #include<Wire.h>
-#include "DHT.h"
+#include "DHT.h" // Libreria Sensor temperatura dth11
 #define DHTTYPE DHT11
-const int DHTPIN =7;
-const int RELE = 8;
+
+// **** PINES DIGITALES  ****
+const int PIN_TEMPERATURA =7 ;
+const int PIN_RELE_COOLER = 4 ;
+const int PIN_RELE_BOMBA = 8 ;
+
+
+// **** PINES ANALOGICOS ****
 const int PIN_HUMEDAD = A0;
 
-DHT dht(DHTPIN,DHTTYPE);
+// **** PINES PWM ****
+const int PIN_LED = 3 ;
+
+DHT dht(PIN_TEMPERATURA,DHTTYPE);
 LiquidCrystal_I2C lcd(0x27,16,2);
+
+// Variables globales
+boolean bomba = false ;
+
 void setup() {
-  // put your setup code here, to run once:
+  
   Serial.begin(9600);
   dht.begin();
-  //pinMode(RELE, OUTPUT);
+  //pinMode(PIN_RELE_COOLER, OUTPUT);
   //==========Seteo Inicial para el setup=========//
   lcd.init();
   lcd.backlight();
   lcd.clear();
   lcd.display();
   //=============================================//
+  pinMode(PIN_LED, OUTPUT);
+  
 }
 
+
 void loop() {
-float temperature = dht.readTemperature();
-///int humedad = analogRead(PIN_HUMEDAD);
-int humedadPorcentaje = map(analogRead(PIN_HUMEDAD), 1023, 0, 0, 100);
-if(temperature>25){
-      pinMode(RELE, OUTPUT);
-      digitalWrite(RELE,LOW);
-      Serial.print("Temperatura:");
-      Serial.println(temperature);
-      lcd.setCursor(0,0);
-      lcd.print("COOLER");
-      lcd.setCursor (0,1);
-      lcd.print("ENCENDIDO");
+  
+  float temperature = dht.readTemperature();
+  // # rango de valores # 0 ~300 muy humedo # 300~700 intermedio # 700~1023 muy seco*
+  // FALTA VERLO EN PORCENTAJE
+  int humedad = analogRead(PIN_HUMEDAD);
+  //int humedadPorcentaje = map(analogRead(PIN_HUMEDAD), 1023, 0, 0, 100);
+  
+  /*Mostrar temperatura y humedad de la tierra*/
+  /*lcd.setCursor(0,0);
+  lcd.print("Temp: "+String(temperature)+"C");
+  lcd.setCursor (0,1);
+  lcd.print("Humedad: "+String(humedad));
+*/
+  Serial.print("Temperatura: ");
+  Serial.println(temperature);
+  Serial.print("Humedad: ");
+  Serial.println(humedad);
+  Serial.println(bomba);      
+  
+  /*if(temperature>25){
+        pinMode(PIN_RELE_COOLER, OUTPUT);
+        digitalWrite(PIN_RELE_COOLER,LOW);
+        Serial.print("Temperatura: ");
+        Serial.println(temperature);        
+        
+        lcd.setCursor(0,0);
+        lcd.print("COOLER");
+        lcd.setCursor (0,1);
+        lcd.print("ENCENDIDO");
+    }
+    else{
+      
+        Serial.print("Temperatura: ");
+        Serial.println(temperature);
+        
+        digitalWrite(PIN_RELE_COOLER,HIGH);
+        lcd.setCursor(0,0);
+        lcd.print("COOLER");
+        lcd.setCursor (0,1);
+        lcd.print("APAGADO"); 
   }
-  else{
-       Serial.print("Temperatura:");
-       Serial.println(temperature);
-       digitalWrite(RELE,HIGH);
-       lcd.setCursor(0,0);
-       lcd.print("COOLER");
-       lcd.setCursor (0,1);
-       lcd.print("APAGADO"); 
- }
+*/
+  if(humedad > 700 ){
+
+      if(bomba == false){
+      pinMode(PIN_RELE_BOMBA, OUTPUT);
+      digitalWrite(PIN_RELE_BOMBA,LOW);
+      bomba = true ;
+      lcd.setCursor(0,0);
+      lcd.print("BOMBA");
+      lcd.setCursor (0,1);
+      lcd.print("ENCENDIDA");
+      //prenderLed();
+      }
+
+      
+      
+  }
+  else if(humedad < 400){
+    
+      Serial.print("muy mojado");
+      if(bomba == true ){
+      
+      digitalWrite(PIN_RELE_BOMBA,HIGH);
+
+      lcd.setCursor(0,0);
+      lcd.print("BOMBA");
+      lcd.setCursor (0,1);
+      lcd.print("APAGADA"); 
+      bomba=false;
+      }
+      
+  }
+  
+}
+
+void prenderLed(){
+
+  int i;
+  for  (i=0;i<256; i++){
+      analogWrite(PIN_LED,i);
+      delay(10);
+  }
+  
+  for  (i=255;i>=0; i--){
+      analogWrite(PIN_LED,i);
+      delay(10);
+  }
+  
 }
